d2f0c37047b3742b264c23ddb4414505429d9653
Byron Troncoso <dazedkun@gmail.com>
2022-09-12 13:31:47 -0400

    Commit rama 1

--- lunesnueveseptiembre/urls.py
+++ lunesnueveseptiembre/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from firstApp import views
+from firstApp import views as app1
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path ("hola/", views.display)
+    path ("hola/", app1.display)
 ]
