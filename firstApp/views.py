d1d5fb7c6f4ea0224f40663f24cf7ce027504099
Byron Troncoso <dazedkun@gmail.com>
2022-09-12 13:22:19 -0400

    Commit rama 1

--- firstApp/views.py
+++ firstApp/views.py
@@ -1,3 +1,13 @@
+from datetime import datetime
 from django.shortcuts import render
-
+from django.http import HttpResponse
+import datetime
 # Create your views here.
+
+def display(request):
+    return HttpResponse ("<h1>Hola Mundo!</h1>")
+
+def displayDateTime(request):
+    dt = datetime.datetime.now()
+    s= "<b>Fecha y hora actual: </b>"+str(dt)
+    return HttpResponse(s)
